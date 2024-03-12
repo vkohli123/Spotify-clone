abde81d79c3f758eecb6b70ffc9b313f8f61b56e
Vedant Kohli <vedant.kohli@yahoo.com>
2024-03-12 13:00:14 +0530

    final

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { MyUserContextProvider } from "@/Hooks/useUser"; 
+
+
+interface UserProviderProps {
+    children: React.ReactNode;
+  }
+  const UserProvider: React.FC<UserProviderProps> = ({
+    children
+  }) => {
+    return ( 
+      <MyUserContextProvider>
+        {children}
+      </MyUserContextProvider>
+     );
+  }
+  export default UserProvider;
